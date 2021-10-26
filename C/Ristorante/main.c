b2179f3aacf0083e19e39abba5ff48928207ed57
Gabri3445 <42178704+Gabri3445@users.noreply.github.com>
2021-10-26 10:49:47 +0200

    Create main.c

---
+++ C/Ristorante/main.c
@@ -0,0 +1,51 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int
+main ()
+{
+  int numPrimo = 0;
+  int numSecondo = 0;
+  int festivo = 0;
+  float tot = 0;
+  float iva = 0;
+  float totiva = 0;
+  float totcosto = 0;
+  const int costoPrimo = 20;
+  const int costoSecondo = 10;
+  const int scontoLav = 5;
+  const int costoOrc = 50;
+
+  printf ("Scrivi il numero di primi ");
+  scanf ("%d", &numPrimo);
+
+  printf ("Scrivi il numero di secondi ");
+  scanf ("%d", &numSecondo);
+
+  printf ("E un giorni festivo? 0/1 ");
+  scanf ("%d", &festivo);
+
+  tot = (numPrimo * costoPrimo) + (numSecondo * costoSecondo);
+  iva = tot * 0.10;
+  totiva = tot + iva;
+  
+  if (festivo == 0)
+    {
+      totcosto = totiva - scontoLav;
+    }
+  else
+    {
+      totcosto = totiva + costoOrc;
+    }
+
+  printf ("Il costo e: %f", totcosto);
+
+}
