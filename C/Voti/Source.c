664ff29553ba98de2cc84064c8fd47d006f467cc
Gabri3445 <gabriele.ope@gmail.com>
2021-11-22 16:15:46 +0100

    Consegna

--- C/Voti/Source.c
+++ C/Voti/Source.c
@@ -1,72 +1,35 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#define NVOTI 5
+#include <stdio.h>
+#define NVOTI 5.00
 
-int main()
+int
+main()
 {
-	int voto0, voto1, voto2, voto3, voto4;
-	float votoMed;
-	int votoMax, votoMin;
-
-	printf("Inserisci il primo voto ");
-	scanf("%d", &voto0);
-	printf("Inserisci il secondo voto ");
-	scanf("%d", &voto1);
-	printf("Inserisci il terzo voto ");
-	scanf("%d", &voto2);
-	printf("Inserisci il quarto voto ");
-	scanf("%d", &voto3);
-	printf("Inserisci il quinto voto ");
-	scanf("%d", &voto4);
-
-	votoMed = (voto0 + voto1 + voto2 + voto3 + voto4) / NVOTI;
-
-	if (voto0 > voto1 && voto0 > voto2 && voto0 > voto3 && voto0 > voto4)
-	{
-		votoMax = voto0;
-	}
-	else if (voto1 > voto2 && voto1 > voto3 && voto1 > voto4)
-	{
-		votoMax = voto1;
-	}
-	else if (voto2 > voto3 && voto2 > voto4)
-	{
-		votoMax = voto2;
-	}
-	else if (voto3 > voto4)
-	{
-		votoMax = voto3;
-	}
-	else
-	{
-		votoMax = voto4;
-	}
-
-
-	if (voto0 < voto1 && voto0 < voto2 && voto0 < voto3 && voto0 < voto4)
-	{
-		votoMin = voto0;
-	}
-	else if (voto1 < voto2 && voto1 < voto3 && voto1 < voto4)
-	{
-		votoMin = voto1;
-	}
-	else if (voto2 < voto3 && voto2 < voto4)
-	{
-		votoMin = voto2;
-	}
-	else if (voto3 < voto4)
-	{
-		votoMin = voto3;
-	}
-	else
-	{
-		votoMin = voto4;
-	}
-
-	printf("La media e' %.2f\n", votoMed);
-	printf("Il voto minimo e' %d\n", votoMin);
-	printf("Il voto massimo e' %d\n", votoMax);
-	system("pause");
+    int voto, i;
+    int votoMin = 10;
+    int votoMax = 0;
+    float votoMed;
+    int sommaMed = 0;
+    i = 0;
+    while (i < NVOTI)
+    {
+        printf("Inserisci un voto ");
+        scanf("%d", &voto);
+        printf("\n");
+        if (voto > votoMax)
+        {
+            votoMax = voto;
+        }
+        if (voto < votoMin)
+        {
+            votoMin = voto;
+        }
+        sommaMed = sommaMed + voto;
+        i++;
+    }
+    votoMed = sommaMed / NVOTI;
+    printf("La media e %.2f \n", votoMed);
+    printf("Il voto massimo e %d \n", votoMax);
+    printf("Il voto minimo e %d\n", votoMin);
+    system("pause");
 }
