4f383c3fa4b0446e0859533da82a8a2003f5c465
Gabri3445 <42178704+Gabri3445@users.noreply.github.com>
2021-11-29 08:39:45 +0100

    Create main.c

---
+++ C/VotiFor/main.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define NVOTI 5.00
+
+int
+main ()
+{
+  int voto, i;
+  int votoMin = 10;
+  int votoMax = 0;
+  float votoMed;
+  int sommaMed = 0;
+  for (i = 0; i < NVOTI; i++)
+    {
+      printf ("Inserisci un voto ");
+      scanf ("%d", &voto);
+      printf ("\n");
+      if (voto > votoMax)
+	{
+	  votoMax = voto;
+	}
+      if (voto < votoMin)
+	{
+	  votoMin = voto;
+	}
+      sommaMed = sommaMed + voto;
+    }
+  votoMed = sommaMed / NVOTI;
+  printf ("La media e %.2f \n", votoMed);
+  printf ("Il voto massimo e %d \n", votoMax);
+  printf ("Il voto minimo e %d", votoMin);
+  system ("pause");
+}
