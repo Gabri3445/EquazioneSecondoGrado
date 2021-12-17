4ba1db8daf574e9537a9c4f9f075e94122d08c31
Gabri3445 <gabriele.ope@gmail.com>
2021-12-17 16:23:24 +0100

    Create Source.c

---
+++ C/Divisori/Consegna/Source.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void main()
+{
+	long int num = 1;
+	do
+	{
+		printf("Scrivi un numero maggiore a zero ");
+		scanf("%d", &num);
+		divisori(num);
+	} while (num > 0);
+	printf("Hai inserito zero o un numero minore a zero\n");
+	system("pause");
+}
+
+int divisori(int num)
+{
+	long int numDiv, numCount;
+	numDiv = num;
+	while (numDiv > 0)
+		{
+			numCount = num % numDiv;
+			if (numCount == 0)
+			{
+				printf("\n%d\n", numDiv);
+			}
+			numDiv--;
+		}
+	printf("\n");
+	return 0;
+}
