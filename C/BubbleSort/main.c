6acdaa4162dc41c951f703aca52a0b018219a1f3
Gabri3445 <42178704+Gabri3445@users.noreply.github.com>
2022-02-10 10:45:05 +0100

    Update main.c

--- C/BubbleSort/main.c
+++ C/BubbleSort/main.c
@@ -1,2 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+int main()
+{
+  int n = 5;
+  int arr[n] = {20, 6, 4, 7, 93};
+}
+
+void printArr(int arr[], int size)
+{
+  for(int i = 0; i < 0; i++)
+  {
+    printf("%d", arr[i];
+  }
+}
