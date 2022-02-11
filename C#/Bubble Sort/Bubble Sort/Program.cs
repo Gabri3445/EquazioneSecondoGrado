ffababb8d9db6bca46699f5b4391fc6650dc169d
Gabri3445 <gabriele.ope@gmail.com>
2022-02-11 19:05:40 +0100

    Bubble sort

---
+++ C#/Bubble Sort/Bubble Sort/Program.cs
@@ -0,0 +1,40 @@
+﻿int arrSize = 0;
+Console.WriteLine("Inserisci quanti elementi casuali vuoi");
+arrSize = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[arrSize];
+for (int i = 0; i < arrSize; i++)
+{
+    arr[i] = Convert.ToInt32(Random.Shared.Next(arrSize + arrSize));
+}
+printArray(arr);
+for (int i = 0; i < 10; i++)
+{
+    Console.Write("\n");
+}
+bubbleSort(arr);
+printArray(arr);
+Console.ReadKey();
+void bubbleSort(int[] arr)
+{
+    int i = 0, j = 0;
+
+    for (i = 0; i < arrSize - 1; i++)
+    {
+        for (j = 0; j < arrSize - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+void printArray(int[] arr)
+{
+    for (int i = 0; i < arrSize; i++)
+    {
+        Console.WriteLine(arr[i]);
+    }
+}
---
+++ C#/DivisoriSharp/DivisoriSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿long num = 0;
+do
+{
+    Console.WriteLine("Scrivi un numero maggiore di zero");
+    num = Convert.ToInt64(Console.ReadLine());
+    divisori(num);
+} while (num > 0);
+
+void divisori(long num)
+{
+    long numCount, numDiv = 0;
+    numDiv = num;
+    while (numDiv > 0)
+    {
+        numCount = num % numDiv;
+        if (numCount == 0)
+        {
+            Console.WriteLine(numDiv);
+        }
+        numDiv--;
+    }
+    return;
+}
