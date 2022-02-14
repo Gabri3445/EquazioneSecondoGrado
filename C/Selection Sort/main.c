6ebe9495ddea4d5799a96e58ffe457be883d5e40
Gabri3445 <42178704+Gabri3445@users.noreply.github.com>
2022-02-14 13:46:05 +0100

    Update main.c

--- C/Selection Sort/main.c
+++ C/Selection Sort/main.c
@@ -1,6 +1,47 @@
 #include <stdlib.h>
 #include <stdio.h>
+#define ARRLENGTH 5
+
+void printArr(int arr[]);
+void selecSort();
+void swap(int* x, int* y);
+
+int arr[ARRLENGTH] = { 6, 60, 2, 40, 1 };
 
 int main ()
 {
+  printArr(arr);
+  selecSort();
+  printArr(arr);
+}
+
+void printArr(int arr[])
+{
+  for (int i = 0; i < ARRLENGTH; i++)
+  {
+    printf("%d", arr[i]);
+  }
+}
+
+void selecSort()
+{
+  int minNum = INT_MAX;
+  for (int i = 0; i < ARRLENGTH - 1; i++)
+  {
+    for (int j = 0; j < ARRLENGTH - 1 - 1; j++)
+    {
+      if (arr[j] < minNum)
+      {
+        minNum = j;
+      }
+    }
+    
+  }
+}
+
+void swap(int* x, int* y)
+{
+  temp = *x;
+  *x = *y;
+  *y = temp;
 }
