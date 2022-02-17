374bf2d686d4462daab4ba250cc63fc656a1fd71
Gabri3445 <42178704+Gabri3445@users.noreply.github.com>
2022-02-17 12:54:57 +0100

    Add files via upload

---
+++ C#/Program.cs
@@ -0,0 +1,65 @@
+﻿char[] alphabet =
+{
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+};
+char[] googleReese =
+{
+    'y',
+    'n',
+    'f',
+    'i',
+    'c',
+    'w',
+    'l',
+    'b',
+    'k',
+    'u',
+    'o',
+    'm',
+    'x',
+    's',
+    'e',
+    'v',
+    'z',
+    'p',
+    'd',
+    'r',
+    'j',
+    'g',
+    't',
+    'h',
+    'a',
+    'q'
+};
+
+int inpNum = Convert.ToInt32(Console.ReadLine());
+string[] input = new string[inpNum];
+for(int i = 0; i < input.Length; i++)
+{
+    input[i] = Console.ReadLine();
+}
