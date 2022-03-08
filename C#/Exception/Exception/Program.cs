ca1bd350fb086b2c0f4d5543a5b306e295f89c14
Gabri3445 <gabriele.ope@gmail.com>
2022-03-08 12:17:22 +0100

    Exception

---
+++ C#/Exception/Exception/Program.cs
@@ -0,0 +1,17 @@
+﻿int foo = 0;
+bool errorlevel = false;
+
+try
+{
+    foo = Convert.ToInt32(Console.ReadLine());
+}
+catch (FormatException ex)
+{
+    errorlevel = true;
+    throw new System.FormatException("Value entered was not a integer", ex);
+}
+finally
+{
+    if (errorlevel == false)
+        Console.WriteLine(foo);
+}
