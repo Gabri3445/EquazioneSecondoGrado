8e79f1ee57842144a6e6a69c4a4b0f555410e2c1
Gabri3445 <gabriele.ope@gmail.com>
2022-04-02 14:34:58 +0200

    Tetris

---
+++ C#/TetrisSong/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+
+Console.Clear();
+Console.WriteLine("Gaming");
+playSong();
+
+
+void playSong()
+{
+    Beep(1320, 500); Beep(990, 250); Beep(1056, 250); Beep(1188, 250); Beep(1320, 125); Beep(1188, 125); Beep(1056, 250); Beep(990, 250); Beep(880, 500); Beep(880, 250); Beep(1056, 250); Beep(1320, 500); Beep(1188, 250); Beep(1056, 250); Beep(990, 750); Beep(1056, 250); Beep(1188, 500); Beep(1320, 500); Beep(1056, 500); Beep(880, 500); Beep(880, 500); System.Threading.Thread.Sleep(250); Beep(1188, 500); Beep(1408, 250); Beep(1760, 500); Beep(1584, 250); Beep(1408, 250); Beep(1320, 750); Beep(1056, 250); Beep(1320, 500); Beep(1188, 250); Beep(1056, 250); Beep(990, 500); Beep(990, 250); Beep(1056, 250); Beep(1188, 500); Beep(1320, 500); Beep(1056, 500); Beep(880, 500); Beep(880, 500); System.Threading.Thread.Sleep(500); playSong();
+}
