c6ef22f007115b6fe51a1a6a70323aaf2e8a6701
Gabri3445 <gabriele.ope@gmail.com>
2022-05-14 02:20:35 +0200

    Initial commit

---
+++ C#/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthorName_Click(object sender, RoutedEventArgs e)
+        {
+            //Display a message box
+            MessageBox.Show(
+                "Programmato da:\n"
+                    + "- Gabriele Lopez (Code and design) \n"
+                    + "- Giovanni Sansonetti & Gabriele Mancini (Original Program and Idea)\n",
+                "Authors"
+            );
+            var uri = "https://github.com/Gabri3445/Scuola";
+            var psi = new System.Diagnostics.ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = uri;
+            System.Diagnostics.Process.Start(psi);
+        }
+    }
+}
