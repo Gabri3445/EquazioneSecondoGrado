b5899d871b3c9bce0605fdbfcd621b90324a1c85
Gabri3445 <gabriele.ope@gmail.com>
2022-05-22 20:00:06 +0200

    Git ignore and Functions

--- C#/Hardware/HardwareCLI/Program.cs
+++ C#/Hardware/HardwareCLI/Program.cs
@@ -1,212 +1,322 @@
-﻿using System;
-using OpenHardwareMonitor.Hardware;
+﻿using OpenHardwareMonitor.Hardware;
 
-namespace PPSU_hwmonitor_c
+namespace OpenHardware
 {
     class Program
     {
-        /**
-         *  Define vars to hold stats
-         **/
-
-        // CPU Temp
-        static float cpuTemp;
-
-        // CPU Usage
-        static float cpuUsage;
-
-        // CPU Power Draw (Package)
-        static float cpuPowerDrawPackage;
-
-        // CPU Frequency
-        static float cpuFrequency;
-
-        // GPU Temperature
-        static float gpuTemp;
-
-        // GPU Usage
-        static float gpuUsage;
-
-        // GPU Core Frequency
-        static float gpuCoreFrequency;
-
-        // GPU Memory Frequency
-        static float gpuMemoryFrequency;
-
-        /**
-         * Init OpenHardwareMonitor.dll Computer Object
-         **/
-
-        static Computer c = new Computer()
-        {
-            GPUEnabled = true,
-            CPUEnabled = true,
-            RAMEnabled = true, // uncomment for RAM reports
-            MainboardEnabled = true, // uncomment for Motherboard reports
-            FanControllerEnabled = true, // uncomment for FAN Reports
-            HDDEnabled = true, // uncomment for HDD Report
-        };
-
-        /**
-         * Pulls data from OHM
-         **/
-
-        static void ReportSystemInfo()
-        {
-            foreach (var hardware in c.Hardware)
-            {
-                if (hardware.HardwareType == HardwareType.CPU)
-                {
-                    // only fire the update when found
-                    hardware.Update();
-
-                    // loop through the data
-                    foreach (var sensor in hardware.Sensors)
-                        if (
-                            sensor.SensorType == SensorType.Temperature
-                            && sensor.Name.Contains("CPU Package")
-                        )
-                        {
-                            // store
-                            cpuTemp = sensor.Value.GetValueOrDefault();
-                            // print to console
-                            Console.WriteLine("cpuTemp: " + sensor.Value.GetValueOrDefault());
-                        }
-                        else if (
-                            sensor.SensorType == SensorType.Load
-                            && sensor.Name.Contains("CPU Total")
-                        )
-                        {
-                            // store
-                            cpuUsage = sensor.Value.GetValueOrDefault();
-                            // print to console
-                            Console.WriteLine("cpuUsage: " + sensor.Value.GetValueOrDefault());
-                        }
-                        else if (
-                            sensor.SensorType == SensorType.Power
-                            && sensor.Name.Contains("CPU Package")
-                        )
-                        {
-                            // store
-                            cpuPowerDrawPackage = sensor.Value.GetValueOrDefault();
-                            // print to console
-                            Console.WriteLine(
-                                "CPU Power Draw - Package: " + sensor.Value.GetValueOrDefault()
-                            );
-                        }
-                        else if (
-                            sensor.SensorType == SensorType.Clock
-                            && sensor.Name.Contains("CPU Core #1")
-                        )
-                        {
-                            // store
-                            cpuFrequency = sensor.Value.GetValueOrDefault();
-                            // print to console
-                            Console.WriteLine("cpuFrequency: " + sensor.Value.GetValueOrDefault());
-                        }
-                }
-
-                // Targets AMD & Nvidia GPUS
-                if (
-                    hardware.HardwareType == HardwareType.GpuAti
-                    || hardware.HardwareType == HardwareType.GpuNvidia
-                )
-                {
-                    // only fire the update when found
-                    hardware.Update();
-
-                    // loop through the data
-                    foreach (var sensor in hardware.Sensors)
-                        if (
-                            sensor.SensorType == SensorType.Temperature
-                            && sensor.Name.Contains("GPU Core")
-                        )
-                        {
-                            // store
-                            gpuTemp = sensor.Value.GetValueOrDefault();
-                            // print to console
-                            Console.WriteLine("gpuTemp: " + sensor.Value.GetValueOrDefault());
-                        }
-                        else if (
-                            sensor.SensorType == SensorType.Load && sensor.Name.Contains("GPU Core")
-                        )
-                        {
-                            // store
-                            gpuUsage = sensor.Value.GetValueOrDefault();
-                            // print to console
-                            Console.WriteLine("gpuUsage: " + sensor.Value.GetValueOrDefault());
-                        }
-                        else if (
-                            sensor.SensorType == SensorType.Clock
-                            && sensor.Name.Contains("GPU Core")
-                        )
-                        {
-                            // store
-                            gpuCoreFrequency = sensor.Value.GetValueOrDefault();
-                            // print to console
-                            Console.WriteLine(
-                                "gpuCoreFrequency: " + sensor.Value.GetValueOrDefault()
-                            );
-                        }
-                        else if (
-                            sensor.SensorType == SensorType.Clock
-                            && sensor.Name.Contains("GPU Memory")
-                        )
-                        {
-                            // store
-                            gpuMemoryFrequency = sensor.Value.GetValueOrDefault();
-                            // print to console
-                            Console.WriteLine(
-                                "gpuMemoryFrequency: " + sensor.Value.GetValueOrDefault()
-                            );
-                        }
-                }
-
-                // ... you can access any other system information you want here
-
-                //ram
-                if (hardware.HardwareType == HardwareType.RAM)
-                {
-                    hardware.Update();
-
-                    foreach (var sensor in hardware.Sensors)
-                    {
-                        if (sensor.SensorType == SensorType.Level) { }
-                    }
-                }
-            }
-        }
-
         static void Main(string[] args)
         {
-            /*c.Open();
-
-            // loop
-            while (true)
-            {
-                Thread.Sleep(500);
-                ReportSystemInfo();
-                Console.WriteLine("\n");
-            }*/
-
             Computer computer = new Computer();
             computer.Open();
             computer.CPUEnabled = true;
             computer.GPUEnabled = true;
             computer.RAMEnabled = true;
 
-            Console.WriteLine("==========================");
+            /*while (true)
+            {
+                Console.WriteLine("==========================");
+                foreach (IHardware hardware in computer.Hardware)
+                {
+                    hardware.Update();
+
+                    if (hardware.HardwareType == HardwareType.RAM)
+                    {
+                        foreach (ISensor sensor in hardware.Sensors)
+                        {
+                            if (sensor.Name == "Memory")
+                            {
+                                Console.WriteLine("RAM: " + sensor.Value.GetValueOrDefault() + "%");
+                            }
+                        }
+                    }
+                    else if (hardware.HardwareType == HardwareType.CPU)
+                    {
+                        foreach (ISensor sensor in hardware.Sensors)
+                        {
+                            if (sensor.Name == "CPU Total")
+                            {
+                                Console.WriteLine(
+                                    "CPU Load : " + sensor.Value.GetValueOrDefault() + "%"
+                                );
+                            }
+                            else if (
+                                sensor.SensorType == SensorType.Clock
+                                && sensor.Name == "CPU Core #1"
+                            )
+                            {
+                                Console.WriteLine(
+                                    "CPU Core #1 : " + sensor.Value.GetValueOrDefault() + "MHz"
+                                );
+                            }
+                            else if (
+                                sensor.SensorType == SensorType.Temperature
+                                && sensor.Name == "CPU Package"
+                            )
+                            {
+                                Console.WriteLine(
+                                    "CPU Package : " + sensor.Value.GetValueOrDefault() + "°C"
+                                );
+                            }
+                        }
+                    }
+                    else if (
+                        hardware.HardwareType == HardwareType.GpuAti
+                        || hardware.HardwareType == HardwareType.GpuNvidia
+                    )
+                    {
+                        foreach (ISensor sensor in hardware.Sensors)
+                        {
+                            if (sensor.SensorType == SensorType.Load)
+                            {
+                                Console.WriteLine(
+                                    "GPU Load: " + sensor.Value.GetValueOrDefault() + "%"
+                                );
+                            }
+                            else if (sensor.SensorType == SensorType.Clock)
+                            {
+                                Console.WriteLine(
+                                    sensor.Name
+                                        + " Clock: "
+                                        + sensor.Value.GetValueOrDefault()
+                                        + "MHz"
+                                );
+                            }
+                            else if (sensor.SensorType == SensorType.Temperature)
+                            {
+                                Console.WriteLine(
+                                    "GPU Temp: " + sensor.Value.GetValueOrDefault() + "°C"
+                                );
+                            }
+                        }
+                    }
+                }
+                Thread.Sleep(500);
+            }*/
+            Console.WriteLine("CPU Load: " + GetCPULoad());
+            Console.WriteLine("CPU Clock: " + GetCPUClock());
+            Console.WriteLine("CPU Temp: " + GetCPUTemp());
+            Console.WriteLine("RAM Percentage: " + GetRAMPercentage());
+            Console.WriteLine("GPU Usage: " + GetGPULoad());
+            Console.WriteLine("GPU Core Clock: " + GetGPUCoreClock());
+            Console.WriteLine("GPU Mem Clock: " + GetGPUMemClock());
+            Console.WriteLine("GPU Temp: " + GetGPUTemp());
+            Console.Read();
+        }
+
+        public static float? GetRAMPercentage()
+        {
+            float perc = 0;
+            Computer computer = new Computer();
+            computer.Open();
+            computer.RAMEnabled = true;
+
             foreach (IHardware hardware in computer.Hardware)
             {
                 hardware.Update();
-                foreach (ISensor sensor in hardware.Sensors)
+
+                if (hardware.HardwareType == HardwareType.RAM)
                 {
-                    if (sensor.Hardware.HardwareType == HardwareType.RAM && sensor.Name == "Memory")
+                    foreach (ISensor sensor in hardware.Sensors)
                     {
-                        Console.WriteLine("RAM: " + sensor.Value.GetValueOrDefault());
+                        if (sensor.Name == "Memory")
+                        {
+                            perc = sensor.Value.GetValueOrDefault();
+                        }
                     }
                 }
             }
+
+            return perc;
+        }
+
+        public static float? GetCPULoad()
+        {
+            float perc = 0;
+            Computer computer = new Computer();
+            computer.Open();
+            computer.CPUEnabled = true;
+
+            foreach (IHardware hardware in computer.Hardware)
+            {
+                hardware.Update();
+
+                if (hardware.HardwareType == HardwareType.CPU)
+                {
+                    foreach (ISensor sensor in hardware.Sensors)
+                    {
+                        if (sensor.Name == "CPU Total")
+                        {
+                            perc = sensor.Value.GetValueOrDefault();
+                        }
+                    }
+                }
+            }
+            return perc;
+        }
+
+        public static float? GetCPUClock()
+        {
+            float clock = 0;
+            Computer computer = new Computer();
+            computer.Open();
+            computer.CPUEnabled = true;
+
+            foreach (IHardware hardware in computer.Hardware)
+            {
+                hardware.Update();
+
+                if (hardware.HardwareType == HardwareType.CPU)
+                {
+                    foreach (ISensor sensor in hardware.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Clock && sensor.Name == "CPU Core #1")
+                        {
+                            clock = sensor.Value.GetValueOrDefault();
+                        }
+                    }
+                }
+            }
+            return clock;
+        }
+
+        public static float? GetCPUTemp()
+        {
+            float temp = 0;
+            Computer computer = new Computer();
+            computer.Open();
+            computer.CPUEnabled = true;
+
+            foreach (IHardware hardware in computer.Hardware)
+            {
+                hardware.Update();
+
+                if (hardware.HardwareType == HardwareType.CPU)
+                {
+                    foreach (ISensor sensor in hardware.Sensors)
+                    {
+                        if (
+                            sensor.SensorType == SensorType.Temperature
+                            && sensor.Name == "CPU Package"
+                        )
+                        {
+                            temp = sensor.Value.GetValueOrDefault();
+                        }
+                    }
+                }
+            }
+            return temp;
+        }
+
+        public static float? GetGPULoad()
+        {
+            float load = 0;
+            Computer computer = new Computer();
+            computer.Open();
+            computer.GPUEnabled = true;
+
+            foreach (IHardware hardware in computer.Hardware)
+            {
+                hardware.Update();
+
+                if (
+                    hardware.HardwareType == HardwareType.GpuAti
+                    || hardware.HardwareType == HardwareType.GpuNvidia
+                )
+                {
+                    foreach (ISensor sensor in hardware.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Load)
+                        {
+                            load = sensor.Value.GetValueOrDefault();
+                        }
+                    }
+                }
+            }
+            return load;
+        }
+
+        public static float? GetGPUCoreClock()
+        {
+            float clock = 0;
+            Computer computer = new Computer();
+            computer.Open();
+            computer.GPUEnabled = true;
+
+            foreach (IHardware hardware in computer.Hardware)
+            {
+                hardware.Update();
+
+                if (
+                    hardware.HardwareType == HardwareType.GpuAti
+                    || hardware.HardwareType == HardwareType.GpuNvidia
+                )
+                {
+                    foreach (ISensor sensor in hardware.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Clock && sensor.Name == "GPU Core")
+                        {
+                            clock = sensor.Value.GetValueOrDefault();
+                        }
+                    }
+                }
+            }
+            return clock;
+        }
+
+        public static float? GetGPUMemClock()
+        {
+            float clock = 0;
+            Computer computer = new Computer();
+            computer.Open();
+            computer.GPUEnabled = true;
+
+            foreach (IHardware hardware in computer.Hardware)
+            {
+                hardware.Update();
+
+                if (
+                    hardware.HardwareType == HardwareType.GpuAti
+                    || hardware.HardwareType == HardwareType.GpuNvidia
+                )
+                {
+                    foreach (ISensor sensor in hardware.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Clock && sensor.Name == "GPU Memory")
+                        {
+                            clock = sensor.Value.GetValueOrDefault();
+                        }
+                    }
+                }
+            }
+            return clock;
+        }
+
+        public static float? GetGPUTemp()
+        {
+            float temp = 0;
+            Computer computer = new Computer();
+            computer.Open();
+            computer.GPUEnabled = true;
+
+            foreach (IHardware hardware in computer.Hardware)
+            {
+                hardware.Update();
+
+                if (
+                    hardware.HardwareType == HardwareType.GpuAti
+                    || hardware.HardwareType == HardwareType.GpuNvidia
+                )
+                {
+                    foreach (ISensor sensor in hardware.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Temperature)
+                        {
+                            temp = sensor.Value.GetValueOrDefault();
+                        }
+                    }
+                }
+            }
+            return temp;
         }
     }
 }
